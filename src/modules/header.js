5e4ca6937a79711388337f64efabf008783492d4
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-07-09 21:33:57 +0300

    refactored: header module

--- src/modules/header.js
+++ src/modules/header.js
@@ -22,33 +22,34 @@ class Header {
   /** private methods */
   _burgerButtonClickHandler({clientX: left, clientY: top}) {
     this._header.toggle('.is-menu-open');
-
-    if( this._header.has('.is-menu-open') ) {
-      $.body.block();
+    this._header.has('.is-menu-open') ? this._openMenu({top, left}) : this._closeMenu()
+  }
+  _openMenu({top, left}) {
+    $.body.block();
       
-      this._menuBgComponent.styles({
-        top: top + 'px',
-        left: left + 'px'
-      });
-      this._header.append(this._menuBgComponent);
+    this._menuBgComponent.styles({
+      top: top + 'px',
+      left: left + 'px'
+    });
+    this._header.append(this._menuBgComponent);
+
+    if( this._isWindowWithTabletWidth() ) {
+      this._signButtons.prepend(this._subscribeComponent);
+      events.emit('init-subscribe-events');
+    }
+  }
+  _closeMenu() {
+    $.body.unblock();
+    this._header.add('.is-menu-close');
+
+    setTimeout(() => {
+      this._header.$('.menu-bg').remove()
+      this._header.remove('.is-menu-close');
 
       if( this._isWindowWithTabletWidth() ) {
-        this._signButtons.prepend(this._subscribeComponent);
-        events.emit('init-subscribe-events');
+        this._signButtons.$('.subscribe').remove();
       }
-    } else {
-      this._header.add('.is-menu-close');
-      $.body.unblock();
-
-      setTimeout(() => {
-        this._header.$('.menu-bg').remove()
-        this._header.remove('.is-menu-close');
-
-        if( this._isWindowWithTabletWidth() ) {
-          this._signButtons.$('.subscribe').remove();
-        }
-      }, 400)
-    }
+    }, 400)
   }
   _isWindowWithTabletWidth() {
     return document.documentElement.clientWidth >= 768;
